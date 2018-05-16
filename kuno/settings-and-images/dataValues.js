1a54afdddf911df12ffe021da8a718820f3833d6
Bachoo <cosmin_cb@hotmail.com>
2018-05-16 18:04:58 -0400

    new stream name

--- kuno/settings-and-images/dataValues.js
+++ kuno/settings-and-images/dataValues.js
@@ -1,1 +1,1 @@
-var values = {"values":["/kuno_suba","@runkunochan","/run_kuno","/kunochan","Join my discord server!","/kunochan","/kuno-chan"]};
+var values = {"values":["/kun0","@runkunochan","/run_kuno","/kunochan","Join my discord server!","/kunochan","/kuno-chan"]};
