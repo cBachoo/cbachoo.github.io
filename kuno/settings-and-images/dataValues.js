1d2c1e421117dbcf52c8d7cd85ad4835c8f1870c
Bachoo <cosmin_cb@hotmail.com>
2018-10-31 11:24:35 -0400

    kuno changed twitter handle

--- kuno/settings-and-images/dataValues.js
+++ kuno/settings-and-images/dataValues.js
@@ -1,1 +1,1 @@
-var values = {"values":["/kun0","@runkunochan","/run_kuno","/kunochan","Join my discord server!","/kunochan","/kuno-chan"]};
+var values = {"values":["/kun0","@artiKun0","/run_kuno","/kunochan","Join my discord server!","/kunochan","/kuno-chan"]};
