f50266e943d2b371b0e614d0f89a896e6d990508
Bachoo <cosmin_cb@hotmail.com>
2020-03-29 11:39:12 -0400

    added kuno twitter

--- kuno/settings-and-images/dataValues.js
+++ kuno/settings-and-images/dataValues.js
@@ -1,1 +1,1 @@
-var values = {"values":["/kun0","@artiKun0","/run_kuno","/kunochan","Join my discord server!","/kunochan","/kuno-chan"]};
+var values = {"values":["/kun0","@KunoMD","/run_kuno","/kunochan","Join my discord server!","/kunochan","/kuno-chan"]};
